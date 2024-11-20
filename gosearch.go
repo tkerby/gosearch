27b589da38147931a2c2e137fba012f89d607649
ibnaleem <ibnaleem@outlook.com>
2024-11-20 10:05:31 +0000

    set User-Agent for MakeRequestWithCookies method

--- gosearch.go
+++ gosearch.go
@@ -118,6 +118,8 @@ func MakeRequestWithCookies(url string, cookies [] Cookie, WebsiteErrorCode int,
 		return
 	}
 
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0")
+
 	for _, cookie := range cookies {
 		cookieObj := &http.Cookie{
 			Name:  cookie.Name,
