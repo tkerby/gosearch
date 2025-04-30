41c8397dad0ea29c6dde6b1181d205308321863f
ibnaleem <ibnaleem@outlook.com>
2025-04-30 15:54:01 +0100

    fix(http): update TLS config to prevent bot detection
    - Added modern TLS 1.3 cipher suites
    
    - Included NextProtos for proper HTTP/2 handling

--- gosearch.go
+++ gosearch.go
@@ -48,16 +48,20 @@ const DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:138.0) Ge
 const VERSION = "v1.0.0"
 
 var tlsConfig = &tls.Config{
-	MinVersion: tls.VersionTLS12,
-	CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-	},
-	CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP256, tls.CurveP384},
+    MinVersion: tls.VersionTLS12,
+    CipherSuites: []uint16{
+        tls.TLS_AES_128_GCM_SHA256,
+        tls.TLS_AES_256_GCM_SHA384,
+        tls.TLS_CHACHA20_POLY1305_SHA256,
+        tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+        tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+        tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+        tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+        tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+        tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+    },
+    CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256, tls.CurveP384},
+    NextProtos: []string{"http/1.1"},
 }
 
 var count atomic.Uint32
